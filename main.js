902ed91ba266fcea357374629d5acf46f90736e7
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-07-12 18:57:50 +0200

    Update

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    title: 'Photo Sorter',
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
