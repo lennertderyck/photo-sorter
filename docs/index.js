9a0823f9c3ffa8c7e1e29720baf85fcb6dd29716
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-08-24 22:35:04 +0200

    Update

--- docs/index.js
+++ docs/index.js
@@ -1,14 +1,21 @@
-import {Api} from 'https://unpkg.com/cutleryjs/dist/js/index.js'
+import {Api, eventCallback, node} from 'https://unpkg.com/cutleryjs/dist/js/index.js'
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const platform = urlParams.get('platform');
 // const version = urlParams.get('v');
 
+window.addEventListener('click', () => {
+    eventCallback('historyBack', () => {
+        window.history.back();
+    })
+})
+
 const core = {
     async init() {
        const version = await core.getCurrentVersion();
-       core.redirect(version, platform);
+       if (platform) core.redirect(version, platform);
+       if (!platform) core.errorMessage();
     },
     
     async getCurrentVersion() {
@@ -19,6 +26,11 @@ const core = {
     
     redirect(version, platform) {
         window.location.replace(`https://github.com/lennertderyck/photo-sorter/releases/download/v${version}/Photo.Sorter-${platform}-x64.zip`)
+    },
+    
+    errorMessage() {
+        const $error = node('[data-label="errorMsg"]')
+        $error.innerHTML = 'Er werd geen platform toegewezen,<br>voeg ?platform=darwin of ?platform=win32 toe aan url'
     }
 }
 
